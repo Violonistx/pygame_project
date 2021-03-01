8991d41c42502c051a30767c5aa6219c39112f9d
Violonist <wiwkw@mail.ru>
2021-03-01 22:57:05 +0500

    IT'S OK

---
+++ main.py
@@ -0,0 +1,97 @@
+import pygame
+from random import randrange as rnd
+
+WIDTH, HEIGHT = 1200, 800
+fps = 60
+# параметры ракетки
+paddle_w = 330
+paddle_h = 35
+paddle_speed = 15
+paddle = pygame.Rect(WIDTH // 2 - paddle_w // 2, HEIGHT - paddle_h - 10, paddle_w, paddle_h)
+# параметры шара
+ball_radius = 20
+ball_speed = 6
+ball_rect = int(ball_radius * 2 ** 0.5)
+ball = pygame.Rect(rnd(ball_rect, WIDTH - ball_rect), HEIGHT // 2, ball_rect, ball_rect)
+dx, dy = 1, -1
+# параметры блоков
+block_list = [pygame.Rect(10 + 120 * i, 10 + 70 * j, 100, 50) for i in range(10) for j in range(4)]
+color_list = [(rnd(30, 256), rnd(30, 256), rnd(30, 256)) for i in range(10) for j in range(4)]
+
+pygame.init()
+sc = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+# фон
+img = pygame.image.load('1.jpg').convert()
+
+
+def detect_collision(dx, dy, ball, rect):
+    if dx > 0:
+        delta_x = ball.right - rect.left
+    else:
+        delta_x = rect.right - ball.left
+    if dy > 0:
+        delta_y = ball.bottom - rect.top
+    else:
+        delta_y = rect.bottom - ball.top
+
+    if abs(delta_x - delta_y) < 10:
+        dx, dy = -dx, -dy
+    elif delta_x > delta_y:
+        dy = -dy
+    elif delta_y > delta_x:
+        dx = -dx
+    return dx, dy
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+    sc.blit(img, (0, 0))
+    # отрисовка
+    [pygame.draw.rect(sc, color_list[color], block) for color, block in enumerate(block_list)]
+    pygame.draw.rect(sc, pygame.Color('darkorange'), paddle)
+    pygame.draw.circle(sc, pygame.Color('white'), ball.center, ball_radius)
+    # движение шара
+    ball.x += ball_speed * dx
+    ball.y += ball_speed * dy
+    # столкновение слева/справа
+    if ball.centerx < ball_radius or ball.centerx > WIDTH - ball_radius:
+        dx = -dx
+    # верхнее столкновение
+    if ball.centery < ball_radius:
+        dy = -dy
+    # столкновение с ракеткой
+    if ball.colliderect(paddle) and dy > 0:
+        dx, dy = detect_collision(dx, dy, ball, paddle)
+        # if dx > 0:
+        #     dx, dy = (-dx, -dy) if ball.centerx < paddle.centerx else (dx, -dy)
+        # else:
+        #     dx, dy = (-dx, -dy) if ball.centerx >= paddle.centerx else (dx, -dy)
+    # столкновение с блоком
+    hit_index = ball.collidelist(block_list)
+    if hit_index != -1:
+        hit_rect = block_list.pop(hit_index)
+        hit_color = color_list.pop(hit_index)
+        dx, dy = detect_collision(dx, dy, ball, hit_rect)
+        # спецэффекты
+        hit_rect.inflate_ip(ball.width * 3, ball.height * 3)
+        pygame.draw.rect(sc, hit_color, hit_rect)
+        fps += 2
+    # победа, поражение
+    if ball.bottom > HEIGHT:
+        print('проиграли.')
+        exit()
+    elif not len(block_list):
+        print('ХОРОШАЯ РАБОТА, СЭР!')
+        exit()
+    # управление
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT] and paddle.left > 0:
+        paddle.left -= paddle_speed
+    if key[pygame.K_RIGHT] and paddle.right < WIDTH:
+        paddle.right += paddle_speed
+    # обновление кадров
+    pygame.display.flip()
+    clock.tick(fps)
